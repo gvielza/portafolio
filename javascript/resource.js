8a1f3ea75ca01b76dcee18cb085721c712edf43a
gvielza <80008338+gvielza@users.noreply.github.com>
2023-01-31 13:44:49 -0300

    particlesjs

--- javascript/resource.js
+++ javascript/resource.js
@@ -1,21 +1,49 @@
-// const image = document.querySelector('.animated-image img');
-
-// document.addEventListener('mousemove', (event) => {
-//   const x = event.clientX / window.innerWidth - 0.5;
-//   const y = event.clientY / window.innerHeight - 0.5;
-  
-//   image.style.transform = `translate3d(${x * 20}px, ${y * 20}px, 0)`;
-// });
-
-
-function je(){
-    alert("je")
-}
-const image = document.querySelector('#myImage');
-
-document.addEventListener('mousemove', (event) => {
-  const x = event.clientX / window.innerWidth;
-  const y = event.clientY / window.innerHeight;
-  
-  image.style.transform = `scale(${x * 2}, ${y * 2})`;
+var current = null;
+document.querySelector('#email').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#password').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+document.querySelector('#submit').addEventListener('focus', function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -730,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '530 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
 });
