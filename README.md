cf2f2f67423542748defeb526d4eeb700cded52d
gvielza <gvielza@users.noreply.github.com>
2025-01-23 16:10:35 +0000

    deploy: c29f925c814c1bfbe514323c3f52d5a88946914c

---
+++ README.md
@@ -0,0 +1,4 @@
+## Portfolio en Boostrap
+
+
+[https://gionnelly.sytes.net](https://gionnelly.sytes.net/)
---
+++ javascript/codigospinner.js
@@ -0,0 +1,4 @@
+window.onload=function(){
+    $('#spinner').fadeOut();
+    $('body').removeClass('hidden');
+}
